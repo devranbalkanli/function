697a1558b7cbc1e19bcb79d7ec341cf086802b1f
User <User@192.168.1.100>
2021-07-14 14:05:37 +0300

    Java'da fonksiyon oluşturma,fonksiyonu çağırma ve fonksiyonu kullanma uygulaması

---
+++ Fonksiyonlar/src/Fonksiyon.java
@@ -0,0 +1,26 @@
+
+ public class Fonksiyon {
+	 
+	 /*yazdýðýmýz fonk her zaman class'ýn süslü parantezleri arasýnda olmalý.
+	  *ve her zaman  Main'in süslü parantezlerinin dýþýnda yazýyoruz fonku 
+	  * ister mainin üstünde istersen de altýnda yaz tamamen bize kalmýþ olan bir þey
+	  * yeter ki classýn süslü parantezleri içinde olsun
+	  */
+	 
+	 
+	public static void f(int x) {       
+		 int result= (x*5)+5;
+		 System.out.println(result);
+		 
+		
+	}
+
+	public static void main(String args []) {
+		
+		f(6);
+		
+	}
+	
+	
+}
+ 
